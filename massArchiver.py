d9abecc8fc9c2318d977ca74dbc938b599e7da3c
Moksh <mokshchitkara@gmail.com>
2026-04-24 16:45:18 -0700

    Initial Commit

---
+++ massArchiver.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# Created by: Moksh Chitkara
+# Last Update: April 24th 2026
+# v0.1.0
+# Copyright (C) 2026  Moksh Chitkara
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Global Variables
+projectManager = resolve.GetProjectManager()
+
